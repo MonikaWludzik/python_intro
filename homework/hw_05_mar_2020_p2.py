d6dd36d8e6ba74978ddef62b8edd9d8d60fbe337
Monika Wludzik <monikawludzik@gmail.com>
2020-03-11 23:01:55 +0100

    tasks for Tuesday

--- homework/hw_05_mar_2020_p2.py
+++ homework/hw_05_mar_2020_p2.py
@@ -1,19 +1,23 @@
 #zadanie 5
 #print(num1/num2/num3/num4)
 
-height1 = input("Input the length of the top and the bottom edge of the rectangle: ")
-width1 = input("Input the length of the left and right edge of the rectangle: ")
-height2 = (int(height1)*("-"))
-print(height2)
+bottom1 = int(input("Input the length of the top and the bottom edge of the rectangle: "))
+side1 = int(input("Input the length of the left and right edge of the rectangle: "))
+bottom2 = (int(bottom1)*("-"))
+print("+" + bottom2 + "+")
 
-width2 = (int(width1)*"|")
-row = str(width2[0] + int(height1)*' ' + width2[1])
-print(row + '\n')
-
-
+side2 = "|" + (" "*(bottom1)) + "|\n"
+print(side2*bottom1, end='')
+print("+" + bottom2 + "+")
 
 
-
+#row = str(width2[0] + int(height1)*' ' + width2[1])
+#print (row)
+#"+" + (" ")
+#"|"
+#4 * ("|" + )
+#print (automatycznie dodaje znak nowej linii
+#print(row + '\n')
 
 
 
